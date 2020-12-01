33768bdffcfa5401984662a56815b4038045bd2f
Andrew Zhao <andrewzhao112@gmail.com>
2020-12-01 18:22:10 +0800

    modified training to own trainer

--- simsiam/main.py
+++ simsiam/main.py
@@ -1,11 +1,18 @@
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
-from data import GaussianBlur, CIFAR10Pairs
+import torch
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch.nn import functional as F
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 import argparse
-import pytorch_lightning as pl
-from pytorch_lightning.loggers import TensorBoardLogger
+from tqdm import tqdm
+
 from model import SimSiam
+from data import GaussianBlur, CIFAR10Pairs
 
 
 parser = argparse.ArgumentParser(description='Train SimSiam')
@@ -30,8 +37,16 @@ parser.add_argument('--knn-t', default=0.1, type=float, help='softmax temperatur
 # misc.
 parser.add_argument('--data_root', default='../data', type=str, help='path to data')
 parser.add_argument('--logs_root', default='logs', type=str, help='path to logs')
+parser.add_argument('--check_point', default='check_point/simsiam.pth', type=str, help='path to model weights')
 
 args = parser.parse_known_args()[0]
+
+def cosine_loss(p, z):
+        z = z.detach()
+        p = F.normalize(p, dim=1)
+        z = F.normalize(z, dim=1)
+        return -(p @ z.T).mean()
+
 
 if __name__ == '__main__':
     """https://github.com/facebookresearch/moco"""
@@ -63,8 +78,61 @@ if __name__ == '__main__':
     parser.add_argument('--classes', default=len(train_data.classes))
     args = parser.parse_args()
 
-    logger = TensorBoardLogger(args.logs_root, name="simsiam")
-    model = SimSiam(args)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    trainer = pl.Trainer(gpus=2, accelerator='ddp', max_epochs=args.epochs, logger=logger, num_sanity_val_steps=0)
-    trainer.fit(model, train_loader, [feature_loader, test_loader])
+    writer = SummaryWriter(args.logs_root)
+    model = SimSiam(args).to(device)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr,
+                                momentum=args.momentum, weight_decay=args.wd)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs // 40)
+
+    for epoch in tqdm(range(args.epochs)):
+        model.train()
+        train_losses = []
+        for x1, x2 in train_loader:
+            x1, x2 = x1.to(device), x2.to(device)
+            z1, z2, p1, p2 = model(x1, x2)
+            loss = (cosine_loss(p1, z2) + cosine_loss(p2, z1)) / 2
+            train_losses.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        writer.add_scalar('Train Loss', sum(train_losses) / len(train_losses), global_step=epoch)
+
+        model.eval()
+        feature_bank, targets = [], []
+        for data, target in feature_loader:
+            data = data.to(device)
+            with torch.no_grad():
+                feature = model(data, istrain=False)
+            feature = F.normalize(feature, dim=1)
+            feature_bank.append(feature)
+            targets.append(target)
+        feature_bank = torch.cat(feature_bank, dim=0)
+        feature_labels = torch.tensor(targets, device=feature_bank.device)
+
+        linear_classifier = LogisticRegression()
+        linear_classifier.fit(feature_bank.cpu().numpy(), feature_labels.cpu().numpy())
+
+        y_preds, y_trues = [], []
+        for data, target in feature_loader:
+            data = data.to(device)
+            with torch.no_grad():
+                feature = model(data, istrain=False)
+            feature = F.normalize(feature, dim=1)
+            y_preds.append(linear_classifier.predict(feature))
+            y_trues.append(target)
+        y_preds = torch.cat(feature_bank, dim=0).cpu().numpy()
+        y_trues = torch.cat(y_trues, dim=0).cpu().numpy()
+        top1acc = accuracy_score(y_trues, y_preds) * 100
+        writer.add_scalar('Top Acc @ 1', top1acc, global_step=epoch)
+
+        tqdm.write('#########################################################')
+        tqdm.write(
+            f'Epoch {epoch + 1}/{args.epochs}, \
+                Train Loss: {sum(train_losses) / len(train_losses):.3f}, \
+                Top Acc @ 1: {top1acc:.3f}'
+        )
+        torch.save(model.state_dict(), args.check_point)
+        scheduler.step()
--- simsiam/model.py
+++ simsiam/model.py
@@ -2,13 +2,9 @@
 
 from torch import nn
 import torchvision
-import torch
-from torch.nn import functional as F
-import pytorch_lightning as pl
-import math
 
 
-class SimSiam(pl.LightningModule):
+class SimSiam(nn.Module):
     def __init__(self, args):
         super().__init__()
         self.args = args
@@ -46,11 +42,6 @@ class SimSiam(pl.LightningModule):
             nn.Linear(args.bottleneck_dim, args.hidden_dim),
         )
 
-        self.feature_bank = []
-        self.total_num, self.total_top1 = 0, 0
-        self.is_feature_data = True
-        self.epoch = 0
-
     def forward(self, x1, x2=None, istrain=True):
         if istrain:
             z1, z2 = self.encoder(x1), self.encoder(x2)
@@ -58,83 +49,3 @@ class SimSiam(pl.LightningModule):
             return z1, z2, p1, p2
         else:
             return self.encoder(x1)
-
-    def training_step(self, train_batch, batch_idx):
-        x1, x2 = train_batch
-        z1, z2, p1, p2 = self.forward(x1, x2)
-        loss = (self._cosineloss(p1, z2) + self._cosineloss(p2, z1)) / 2
-        self.log('train_loss', loss)
-        return loss
-
-    def train_epoch_end(self, train_losses):
-        self.logger.experiment.add_scalar(
-                'train loss',
-                sum(train_losses) / len(train_losses),
-                global_step=self.epoch)
-
-    def validation_step(self, batch, batch_idx, dataloader_idx):
-        data, target = batch
-        feature = self.forward(data, istrain=False)
-        feature = F.normalize(feature, dim=1)
-
-        if dataloader_idx == 0:
-            self.feature_bank.append(feature)
-            return
-        else:
-            if batch_idx == 0:
-                # complete featurebank & setup
-                self.feature_bank = torch.cat(self.feature_bank, dim=0).t().contiguous()
-                self.feature_labels = torch.tensor(self.args.targets, device=self.feature_bank.device)
-                self.is_feature_data = False
-            # pred_labels = [self.knn_predict(f, self.args.knn_k, self.args.knn_t) for f in feature]
-            pred_labels = self.knn_predict(feature, self.args.knn_k, self.args.knn_t)
-            self.total_num += data.size(0)
-            self.total_top1 += (pred_labels[:, 0] == target).float().sum().item()
-            return self.total_top1 / self.total_num * 100
-
-    def validation_epoch_end(self, top1_acc):
-        if not self.is_feature_data:
-            # resetting & logging
-            top1_acc = top1_acc[1]
-            self.feature_bank = []
-            self.total_num, self.total_top1 = 0, 0
-            self.log('top 1 accuracy', sum(top1_acc) / len(top1_acc))
-            self.logger.experiment.add_scalar(
-                'top 1 accuracy',
-                sum(top1_acc) / len(top1_acc),
-                global_step=self.epoch)
-            self.is_feature_data = True
-            self.epoch += 1
-
-    def knn_predict(self, feature, knn_k, knn_t):
-        # compute cos similarity between each feature vector and feature bank ---> [B, N]
-        sim_matrix = torch.mm(feature, self.feature_bank)
-        # [B, K]
-        sim_weight, sim_indices = sim_matrix.topk(k=knn_k, dim=-1)
-        # free memory
-        sim_matrix = None
-        # [B, K]
-        sim_labels = torch.gather(self.feature_labels.expand(feature.size(0), -1), dim=-1, index=sim_indices)
-        sim_weight = (sim_weight / knn_t).exp()
-
-        # counts for each class
-        one_hot_label = torch.zeros(feature.size(0) * knn_k, self.args.classes, device=sim_labels.device)
-        # [B*K, C]
-        one_hot_label = one_hot_label.scatter_(dim=-1, index=sim_labels.view(-1, 1), value=1.0)
-        # weighted score ---> [B, C]
-        pred_scores = torch.sum(one_hot_label.view(feature.size(0), -1, self.args.classes) * sim_weight.unsqueeze(-1), dim=1)
-
-        pred_labels = pred_scores.argsort(dim=-1, descending=True)
-        return pred_labels
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.args.lr,
-                                    momentum=self.args.momentum, weight_decay=self.args.wd)
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.args.epochs // 40)
-        return [optimizer], [scheduler]
-
-    def _cosineloss(self, p, z):
-        z = z.detach()
-        p = F.normalize(p, dim=1)
-        z = F.normalize(z, dim=1)
-        return -(p @ z.T).mean()
