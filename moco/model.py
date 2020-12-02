2d5cbcf9b03edb8540ac5475f84566999fc83288
Andrew Zhao <andrewzhao112@gmail.com>
2020-12-02 17:22:20 +0800

    added simpler model implementation

--- moco/model.py
+++ moco/model.py
@@ -1,7 +1,15 @@
 from torch import nn
 from torch.nn import functional as F
 import torchvision
+from networks.layers import ConvNormAct
 
+class Print(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        print(x.size())
+        return x
 
 class MoCo(nn.Module):
     def __init__(self, args):
@@ -17,6 +25,15 @@ class MoCo(nn.Module):
             self.encoder = torchvision.models.resnet101(progress=False)
         elif args.backbone == 'resnet152':
             self.encoder = torchvision.models.resnet152(progress=False)
+        elif args.backbone == 'basic':
+            self.encoder = nn.Sequential(
+                ConvNormAct(3, 32, mode='down'),
+                ConvNormAct(32, 64, mode='down'),
+                ConvNormAct(64, 128, mode='down'),
+                nn.AdaptiveAvgPool2d(1),
+                nn.Flatten()
+            )
+            self.encoder.fc = nn.Linear(128, 128)
         else:
             raise NotImplementedError
         fc = [nn.Linear(self.encoder.fc.in_features, args.feature_dim)]
